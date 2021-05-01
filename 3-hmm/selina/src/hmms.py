66a2b8d2476e7f3032595abd8d67e70739394872
Selina Feitl <selina.feitl@infoteam.de>
2021-05-01 17:14:24 +0200

    first try for IWR

--- 3-hmm/selina/src/hmms.py
+++ 3-hmm/selina/src/hmms.py
@@ -24,11 +24,24 @@ def load_fts(digit: int, spk: str, n: int):
     mfcc = librosa.feature.mfcc(y=y, sr=sr, hop_length=int(0.010*sr), n_fft=int(0.025*sr), n_mfcc=13)
     return mfcc
 
-test = load_fts(0, 'george', 3)
-# load data files and extract features
+def load():
+    mfccs = {}
+    for speaker in speakers:
+        digit_list = [] 
+        for digit in digits:
+            rec_list = []
+            for i in range(0, 50):
+                mfcc = load_fts(digit, speaker, i)
+                rec_list.append(mfcc)
+            digit_list.append(rec_list)
+        mfccs[speaker] = digit_list
+    return mfccs
 
+mfccs = load()
+# %%
+# Sample output
+mfccs['george'][0][0]
 # %% 
-
 # implement a 6-fold cross-validation (x/v) loop so that each speaker acts as
 # test speaker while the others are used for training
 
@@ -45,6 +58,49 @@ test = load_fts(0, 'george', 3)
 
 # compute and display the confusion matrix
 # https://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+
+def prepare_train_data(train_mfccs: dict):
+    train_data = {}
+    for i in range(0,len(digits)):
+        digit_recs = []
+        for speaker_data in train_mfccs.keys():
+            for j in range(0, nr):
+                digit_recs.append(train_mfccs[speaker_data][i][j])
+        flatlist = [item for sublist in digit_recs for item in sublist]
+        y = np.concatenate(flatlist, axis=0)
+        train_data[i] = y.reshape(-1, 1)
+    return train_data
+
+def train_hmms(train_mfccs: dict):
+    trained_hmms={}
+    for label in train_mfccs.keys():
+        model = hmm.GaussianHMM(n_components=3, covariance_type="full", n_iter=10)
+        model.fit(train_mfccs[label])  
+        trained_hmms[label] = model
+    return trained_hmms
+
+def test_hmms(trained_hmms:dict, test_mfccs: list):
+    for i in range(0, len(test_mfccs)):
+        flatlist = [item for sublist in test_mfccs[i] for item in sublist]
+        y = np.concatenate(flatlist, axis=0)
+        features = y.reshape(-1, 1)
+        scoreList = {}
+        for model_label in trained_hmms.keys():
+            model = trained_hmms[model_label]
+            score = model.score(features)
+            scoreList[model_label] = score
+        predict = max(scoreList, key=scoreList.get)
+        print("True label ", i, ": Predicted Label: ", predict)
+
+def cross_validation(mfccs: dict):    
+    for speaker in speakers: 
+        train_mfccs = mfccs.copy()
+        del train_mfccs[speaker]
+        train_data = prepare_train_data(train_mfccs)
+        trained_hmms = train_hmms(train_data)
+        test_hmms(trained_hmms, mfccs[speaker])
+cross_validation(mfccs)
+
 
 # %%
 # display the overall confusion matrix
